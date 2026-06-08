3f0eaae1450aeb00235fe3c48f91e529ac586389
João Martins <joaomartins72021@gmail.com>
2026-06-08 11:51:00 -0300

    feat: add swagger for documentions endpoints

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
+
+#[OA\Info(
+    title: 'AppAlways API',
+    version: '1.0.0',
+    description: 'API de Checkout - Teste Técnico AlwaysFit'
+)]
+#[OA\Server(
+    url: '/',
+    description: 'Local'
+)]
+
 
 abstract class Controller
 {
